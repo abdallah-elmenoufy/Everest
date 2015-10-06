2dad8545e8aafdeebc407153052c692d9ba5a606
Abdallah ElMenoufy <abdallah.elmenoufy@gmail.com>
2015-10-07 00:54:28 +0200

    Added CollectionView and Table View cells details

---
+++ Everest/FlickrPhotoCollectionViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  FlickrPhotoCollectionViewCell.swift
+//  Everest
+//
+//  Created by Abdallah ElMenoufy on 10/7/15.
+//  Copyright © 2015 Abdallah ElMenoufy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FlickrPhotoCollectionViewCell: UICollectionViewCell {
+    
+    //MARK: - Properties
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    
+    override func prepareForReuse() {
+        
+        super.prepareForReuse()
+        
+        if imageView.image == nil {
+            
+            activityIndicatorView.startAnimating()
+        }
+    }
+}
---
+++ Everest/FlickrPhotoCollectionViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FlickrPhotoCollectionViewController.swift
+//  Everest
+//
+//  Created by Abdallah ElMenoufy on 10/7/15.
+//  Copyright © 2015 Abdallah ElMenoufy. All rights reserved.
+//
+
+import CoreData
+import MapKit
+import UIKit
+
+class FlickrPhotoCollectionViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    @IBOutlet weak var mapView:             MKMapView!
+    @IBOutlet weak var collectionView:      UICollectionView!
+    @IBOutlet weak var newCollectionButton: UIButton!
+    @IBOutlet weak var noImagesLabel:       UILabel!
+
+
+
+
+
+}
